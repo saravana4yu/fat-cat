4143bdbbb48f218d00b4476c0144aa124b859237
saravana4yu <saravanananthaa@gmail.com>
2023-08-24 17:30:38 +0530

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,35 @@
+import { cn } from "@/lib/utils";
+import "./globals.css";
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import Providers from "@/components/Providers";
+import Navbar from "@/components/Navbar";
+import { Toaster } from "@/components/ui/toaster";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+export const metadata: Metadata = {
+  title: "Fat Cat",
+  description: "Quiz yourself on anything!",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={cn(poppins.className, "antialiased min-h-screen pt-16")}>
+        <Providers>
+          <Navbar />
+          {children}
+          <Toaster />
+        </Providers>
+      </body>
+    </html>
+  );
+}
